795b22adf55b4bcbe6aea58d743439b0d27df1d4
sulfranc <sulfranc@gmail.com>
2022-05-23 13:52:22 -0400

    done

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+
+function handleChange(){
+    console.log("Entering password...")
+}
+
+function Keypad(){
+    return(
+        <input type="password" 
+            name="keypad" 
+            placeholder="Enter number"
+            onChange={handleChange}
+        ></input>
+    )
+}
+
+export default Keypad
